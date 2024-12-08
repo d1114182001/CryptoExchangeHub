f5c7c0450a9e599953774f482228ace675f1b662
Huang-Wei-Lun <ygww407@gmail.com>
2024-12-08 18:54:52 +0800

    12/08進度

--- app/Http/Controllers/CryptoChangeHubController.php
+++ app/Http/Controllers/CryptoChangeHubController.php
@@ -13,7 +13,7 @@ class CryptoChangeHubController extends Controller
      */
     public function index()
     {
-        return view("CryptoChangeHub.php");
+        return view("cryptoexChangeHub.index");
     }
 
     /**
---
+++ app/Models/cryptoChangeHub.php
@@ -0,0 +1,11 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class cryptoChangeHub extends Model
+{
+    use HasFactory;
+}
